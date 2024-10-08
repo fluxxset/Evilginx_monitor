b009c2aa2bbf74dbb7a631bbc39b6982eaf5e7fc
fluxxset <fluxx0303@gmail.com>
2024-10-08 14:01:16 +0530

    fixed the maximum size of a Telegram message

--- telegram_sender.go
+++ telegram_sender.go
@@ -8,6 +8,9 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+// MaxTelegramMessageLength is the maximum size of a Telegram message (4096 characters).
+const MaxTelegramMessageLength = 4096
+
 func sendTelegramNotification(chatID string, token string, message string) {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
@@ -15,20 +18,36 @@ func sendTelegramNotification(chatID string, token string, message string) {
 		return
 	}
 
-	// Convert chatID string to int64 if necessary
+	// Convert chatID string to int64
 	chatIDInt, err := strconv.ParseInt(chatID, 10, 64)
 	if err != nil {
 		log.Println("Invalid chat ID format:", err)
 		return
 	}
 
-	msg := tgbotapi.NewMessage(chatIDInt, message) // Use NewMessage for direct messages to users or groups
-	msg.ParseMode = "Markdown"                     // Enable Markdown formatting
+	// Split and send the message in chunks
+	sendInChunks(bot, chatIDInt, message)
+}
 
-	_, err = bot.Send(msg)
-	if err != nil {
-		log.Println("Failed to send message:", err)
-	} else {
-		fmt.Println("Message sent using Telegram")
+func sendInChunks(bot *tgbotapi.BotAPI, chatID int64, message string) {
+	// Split the message into chunks if it exceeds MaxTelegramMessageLength
+	for len(message) > 0 {
+		part := message
+		if len(part) > MaxTelegramMessageLength {
+			part = message[:MaxTelegramMessageLength]    // Get the first 4096 characters
+			message = message[MaxTelegramMessageLength:] // Remaining part of the message
+		} else {
+			message = "" // We're done with the whole message
+		}
+
+		msg := tgbotapi.NewMessage(chatID, part)
+		msg.ParseMode = "Markdown" // Enable Markdown formatting if needed
+
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Println("Failed to send message part:", err)
+			return
+		}
+		fmt.Println("Message part sent")
 	}
 }
