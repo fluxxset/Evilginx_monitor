0694afee0b6fb3366395be92046c79de78bce3b2
psydo72 <psydo72@proton.me>
2024-11-15 09:25:51 -0500

    Update discord_sender.go

--- discord_sender.go
+++ discord_sender.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-func sendDiscordNotification(userID string, token string, message string) {
+// sendDiscordNotification sends a message and an attachment to a user on Discord.
+func sendDiscordNotification(userID string, token string, message string, attachmentPath string) {
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
@@ -30,11 +32,32 @@ func sendDiscordNotification(userID string, token string, message string) {
 		return
 	}
 
-	// Send the message to the DM channel
-	_, err = dg.ChannelMessageSend(channel.ID, message)
+	// Prepare the attachment
+	// If attachmentPath is not empty, attach the file
+	var files []*discordgo.File
+	if attachmentPath != "" {
+		file, err := os.Open(attachmentPath)
+		if err != nil {
+			log.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close()
+
+		// Add the file to the files slice
+		files = append(files, &discordgo.File{
+			Name:   "attachment", // You can specify the file name here
+			Reader: file,
+		})
+	}
+
+	// Send the message with the attachment to the DM channel
+	_, err = dg.ChannelMessageSendComplex(channel.ID, &discordgo.MessageSend{
+		Content: message,
+		Files:   files, // Attach files here
+	})
 	if err != nil {
 		log.Println("Failed to send message:", err)
 	} else {
-		fmt.Println("Message sent using Discord")
+		fmt.Println("Message and attachment sent successfully!")
 	}
 }
